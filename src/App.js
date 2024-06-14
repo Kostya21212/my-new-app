e509e552358550ac490cb02d1a665dfac68e7e1c
Onischenko Kostyantyn <Onischenko411@gmail.com>
2024-06-14 20:49:57 +0300

    34354

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
 
+import React from 'react';
+import HeaderFunction from './components/Header/Header';
+import LeftSideNavigation from './components/LeftSide/LeftSideNavigation';
+import MiddleContainer from './components/MiddleContainer/MiddleContainer';
+import Footer from './components/footer/Footer';
+import './App.css';
+import './index.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className='bg vh-100 img-fluid'>
+    <HeaderFunction />
+      <LeftSideNavigation />
+      <MiddleContainer />
+      <Footer />
+   </div>
   );
 }
 
